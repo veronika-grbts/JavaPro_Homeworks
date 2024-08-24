3d4dcdc1d674526488c21cfc1253b4cc12fb6e4f
veronika-grbts <veranikadnepr@gmail.com>
2024-08-24 10:04:32 +0300

    lesson 4

--- src/main/java/lesson4/Customer.java
+++ src/main/java/lesson4/Customer.java
@@ -1,5 +1,19 @@
 package lesson4;
 
 public class Customer {
+    private String name;
+    private String phone;
 
+    public Customer(String name, String phone) {
+        this.phone = phone;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
